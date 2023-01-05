0e7e4c34c8f674029cf2a0882ea063647f33f706
GottaGinger <sierratmorris@gmail.com>
2023-01-04 20:16:38 -0500

    message

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -4,6 +4,7 @@ public class MidTerm {
 	public static void main(String[] args) {
 		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
 
+		System.out.println("And among the land there was a plight, eating away at the life until it was naught but a husk.")
 	}
 
 }
